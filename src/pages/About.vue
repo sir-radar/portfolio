57630126271a129b044bb45eb1246f84e8952f2f
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-09-16 21:38:12 +0100

    portfolio page done

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,9 +3,7 @@
     <section id="details" class="mb-12">
       <h3 class="uppercase text-white text-base font-bold mb-2">About me</h3>
       <BigDivider class="mb-8" />
-      <h1 class="uppercase text-white text-title font-semibold">
-        Samson Nwokike
-      </h1>
+      <h1 class="uppercase text-white text-title font-semibold">Samson Nwokike</h1>
       <p class="uppercase text-muted text-xl mb-2">Full Stack Developer</p>
       <div class="flex flex-col md:flex-row w-full mb-3">
         <div class="w-full md:w-1/2 mb-15 md:mb-0 md:pr-15 text-muted">
@@ -29,32 +27,26 @@
                 <li class="md:border-b md:border-line-color w-full">
                   <span
                     class="block text-center py-2 w-full md:w-3/12 md:inline-block md:border-r font-semibold md:border-line-color text-white"
-                    >Name</span
-                  >
+                  >Name</span>
                   <span
                     class="py-2 text-center text-muted inline-block w-full md:w-9/12"
-                    >Carlos Smith</span
-                  >
+                  >Samson Nwokike</span>
                 </li>
                 <li class="md:border-b md:border-line-color w-full">
                   <span
                     class="block text-center py-2 w-full md:w-3/12 md:inline-block md:border-r font-semibold md:border-line-color text-white"
-                    >Birth</span
-                  >
+                  >Birth</span>
                   <span
                     class="py-2 text-center text-muted inline-block w-full md:w-9/12"
-                    >March 26, 1993</span
-                  >
+                  >March 26, 1993</span>
                 </li>
-                <li class="md:border-b md:border-line-color w-full">
+                <li class="w-full">
                   <span
                     class="block text-center py-2 w-full md:w-3/12 md:inline-block md:border-r font-semibold md:border-line-color text-white"
-                    >City</span
-                  >
+                  >City</span>
                   <span
                     class="py-2 text-center text-muted inline-block w-full md:w-9/12"
-                    >Lagos, Nigeria</span
-                  >
+                  >Lagos, Nigeria</span>
                 </li>
               </ul>
             </div>
@@ -67,32 +59,26 @@
                 <li class="md:border-b md:border-line-color w-full">
                   <span
                     class="block text-center py-2 w-full md:w-3/12 md:inline-block md:border-r font-semibold md:border-line-color text-white"
-                    >Email</span
-                  >
+                  >Email</span>
                   <span
                     class="py-2 text-center text-muted inline-block w-full md:w-9/12"
-                    >nwokikesamson@gmail.com</span
-                  >
+                  >nwokikesamson@gmail.com</span>
                 </li>
                 <li class="md:border-b md:border-line-color w-full">
                   <span
                     class="block text-center py-2 w-full md:w-3/12 md:inline-block md:border-r font-semibold md:border-line-color text-white"
-                    >Phone</span
-                  >
+                  >Phone</span>
                   <span
                     class="py-2 text-center text-muted inline-block w-full md:w-9/12"
-                    >+234 9032326238</span
-                  >
+                  >+234 9032326238</span>
                 </li>
-                <li class="md:border-b md:border-line-color w-full">
+                <li class="w-full">
                   <span
                     class="block text-center py-2 w-full md:w-3/12 md:inline-block md:border-r font-semibold md:border-line-color text-white"
-                    >Skype</span
-                  >
+                  >Skype</span>
                   <span
                     class="py-2 text-center text-muted inline-block w-full md:w-9/12"
-                    >live:nwokikesamson</span
-                  >
+                  >live:nwokikesamson</span>
                 </li>
               </ul>
             </div>
@@ -102,36 +88,26 @@
     </section>
 
     <section id="skills" class="mb-12">
-      <h3 class="uppercase text-white text-base font-bold mb-2">
-        Professional Skills
-      </h3>
+      <h3 class="uppercase text-white text-base font-bold mb-2">Professional Skills</h3>
       <BigDivider class="mb-8" />
 
-      <div
-        class="flex flex-col md:flex-row relative items-center justify-center"
-      >
+      <div class="flex flex-col md:flex-row relative items-center justify-center">
         <ul class="w-full md:w-11/12">
           <li>
             <h6 class="uppercase font-semibold mb-2 text-white">Html-css</h6>
-            <div
-              class="shadow-app-inner h-4 mb-4 rounded-3 flex items-center px-2"
-            >
+            <div class="shadow-app-inner h-4 mb-4 rounded-3 flex items-center px-2">
               <div class="w-3/4 h-7 bg-white rounded-2"></div>
             </div>
           </li>
           <li>
             <h6 class="uppercase font-semibold mb-2 text-white">Html-css</h6>
-            <div
-              class="shadow-app-inner h-4 mb-4 rounded-3 flex items-center px-2"
-            >
+            <div class="shadow-app-inner h-4 mb-4 rounded-3 flex items-center px-2">
               <div class="w-3/4 h-7 bg-white rounded-2"></div>
             </div>
           </li>
           <li>
             <h6 class="uppercase font-semibold mb-2 text-white">Html-css</h6>
-            <div
-              class="shadow-app-inner h-4 mb-4 rounded-3 flex items-center px-2"
-            >
+            <div class="shadow-app-inner h-4 mb-4 rounded-3 flex items-center px-2">
               <div class="w-3/4 h-7 bg-white rounded-2"></div>
             </div>
           </li>
@@ -147,17 +123,13 @@
       </div>
     </section>
 
-    <section id="experience">
-      <h3 class="uppercase text-white text-base font-bold mb-2">
-        EXPERIENCE
-      </h3>
+    <section id="experience" class="mb-12">
+      <h3 class="uppercase text-white text-base font-bold mb-2">EXPERIENCE</h3>
       <BigDivider class="mb-8" />
 
       <div class="time relative">
         <div class="shadow-app h-53 w-53 absolute rounded-2 bg-app-black">
-          <div
-            class="flex justify-center items-center hover:shadow-app-inner rounded-2 h-53 w-53"
-          >
+          <div class="flex justify-center items-center hover:shadow-app-inner rounded-2 h-53 w-53">
             <div class="w-6">
               <Case style="fill: #ead22d; stroke: #ead22d;" />
             </div>
@@ -166,16 +138,12 @@
         <div class="pt-16">
           <div class="time-content relative pl-16 pt-5">
             <div>
-              <h6 class="font-semibold text-base text-white mb-1">
-                WebDesigner – Mutation Media
-              </h6>
+              <h6 class="font-semibold text-base text-white mb-1">WebDesigner – Mutation Media</h6>
               <div class="text-muted text-sm mb-1">
-                <span
-                  ><fa class="w-3 inline-block" icon="calendar"></fa> Feb 15th -
-                  <span class="px-2 py-1 shadow-app-inner rounded-2"
-                    >Current</span
-                  ></span
-                >
+                <span>
+                  <fa class="w-3 inline-block" icon="calendar"></fa>Feb 15th -
+                  <span class="px-2 py-1 shadow-app-inner rounded-2">Current</span>
+                </span>
               </div>
               <p class="text-muted">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
@@ -187,16 +155,12 @@
           </div>
           <div class="time-content relative pl-16 pt-5">
             <div>
-              <h6 class="font-semibold text-base text-white mb-1">
-                WebDesigner – Mutation Media
-              </h6>
+              <h6 class="font-semibold text-base text-white mb-1">WebDesigner – Mutation Media</h6>
               <div class="text-muted text-sm mb-1">
-                <span
-                  ><fa class="w-3 inline-block" icon="calendar"></fa> Feb 15th -
-                  <span class="px-2 py-1 shadow-app-inner rounded-2"
-                    >Current</span
-                  ></span
-                >
+                <span>
+                  <fa class="w-3 inline-block" icon="calendar"></fa>Feb 15th -
+                  <span class="px-2 py-1 shadow-app-inner rounded-2">Current</span>
+                </span>
               </div>
               <p class="text-muted">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
@@ -208,16 +172,47 @@
           </div>
           <div class="time-content relative pl-16 pt-5">
             <div>
-              <h6 class="font-semibold text-base text-white mb-1">
-                WebDesigner – Mutation Media
-              </h6>
+              <h6 class="font-semibold text-base text-white mb-1">WebDesigner – Mutation Media</h6>
               <div class="text-muted text-sm mb-1">
-                <span
-                  ><fa class="w-3 inline-block" icon="calendar"></fa> Feb 15th -
-                  <span class="px-2 py-1 shadow-app-inner rounded-2"
-                    >Current</span
-                  ></span
-                >
+                <span>
+                  <fa class="w-3 inline-block" icon="calendar"></fa>Feb 15th -
+                  <span class="px-2 py-1 shadow-app-inner rounded-2">Current</span>
+                </span>
+              </div>
+              <p class="text-muted">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                ut sint voluptas rem? At nisi obcaecati placeat adipisci nobis?
+                Quidem rerum molestias ipsam vero fugiat sint porro in maxime
+                id?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="education" class="mb-12">
+      <h3 class="uppercase text-white text-base font-bold mb-2">Education</h3>
+      <BigDivider class="mb-8" />
+
+      <div class="time relative">
+        <div class="shadow-app h-53 w-53 absolute rounded-2 bg-app-black">
+          <div class="flex justify-center items-center hover:shadow-app-inner rounded-2 h-53 w-53">
+            <div class="w-6">
+              <Education style="fill: #ead22d; stroke: #ead22d;" />
+            </div>
+          </div>
+        </div>
+        <div class="pt-16">
+          <div class="time-content relative pl-16 pt-5">
+            <div>
+              <h6
+                class="font-semibold text-base text-white mb-1"
+              >Bachelor Degree - Nnadi Azikiwe University</h6>
+              <div class="text-muted text-sm mb-1">
+                <span>
+                  <fa class="w-3 inline-block" icon="calendar"></fa>Feb 2011 - Aug 2015
+                </span>
               </div>
               <p class="text-muted">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
@@ -236,42 +231,42 @@
   </div>
 </template>
 <script>
-// import Divider from "@/components/Divider";
-import BigDivider from '@/components/BigDivider';
-import Case from '@/assets/images/svg/case.svg';
-import BottomLinks from '@/components/BottomLinks';
+import BigDivider from "@/components/BigDivider";
+import Case from "@/assets/images/svg/case.svg";
+import Education from "@/assets/images/svg/education.svg";
+import BottomLinks from "@/components/BottomLinks";
 export default {
-  name: 'aboutPage',
+  name: "aboutPage",
   components: {
-    // Divider,
     BigDivider,
     Case,
     BottomLinks,
+    Education,
   },
 };
 </script>
 <style lang="scss" scoped>
 .time::before {
-  background: #2c2d30;
-  box-shadow: inset 3px 3px 3px #202125, inset -3px -3px 3px #353535;
-  bottom: 0;
-  content: '';
+  @apply shadow-app-inner;
+  @apply bg-app-black;
+  @apply bottom-0;
+  content: "";
   left: 1.375rem;
-  position: absolute;
+  @apply absolute;
   top: 0.0625rem;
   width: 0.5rem;
   height: 100%;
-  border-radius: 3em;
+  @apply rounded-3;
 }
 .time-content::before {
   background: #f56c6d;
-  content: '';
+  content: "";
   height: 0.625rem;
   left: 1.5625rem;
-  position: absolute;
-  text-align: center;
-  top: 1.5rem;
+  @apply absolute;
+  @apply text-center;
+  @apply top-16;
   width: 0.25rem;
-  border-radius: 2em;
+  @apply rounded-2;
 }
 </style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an portfolio page</h1>
+  <div class="portfolio">
+    <section id="details" class="mb-12">
+      <h3 class="uppercase text-white text-base font-bold mb-2">My works</h3>
+      <BigDivider class="mb-8" />
+    </section>
   </div>
 </template>
+<script>
+import BigDivider from "@/components/BigDivider";
+export default {
+  name: "aboutPage",
+  components: {
+    BigDivider,
+  },
+};
+</script>
