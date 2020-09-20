e75d12f4db42347a03a802e30a944bd9da5f3312
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-09-20 23:53:48 +0100

    contact page design in progress

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an contact page</h1>
+    <section id="header">
+      <h3 class="uppercase text-white text-base font-bold mb-2">
+        Map location
+      </h3>
+      <BigDivider class="mb-8" />
+      <div class="w-full py-4 relative">
+        <div class="shadow-app-inner p-2 h-388 rounded-1">
+          <div class="shadow-app w-full p-1 h-full rounded-1"></div>
+        </div>
+      </div>
+    </section>
+    <section class="info">
+      <h3 class="uppercase text-white text-base font-bold mb-2">
+        Contact info
+      </h3>
+      <BigDivider class="mb-8" />
+    </section>
   </div>
 </template>
+<script>
+import BigDivider from '@/components/BigDivider';
+export default {
+  name: 'aboutPage',
+  components: {
+    BigDivider,
+  },
+};
+</script>
