5276193bc42c28dbb94bb6f8551fdb0dd354999f
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-12-24 03:33:12 +0100

    update about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -13,16 +13,10 @@
       <p class="mb-2 text-xl text-muted">Full Stack Developer</p>
       <div class="flex flex-col w-full mb-3 md:flex-row">
         <div class="w-full md:w-1/2 mb-15 md:mb-0 md:pr-15 text-muted">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-          dignissimos reiciendis consequatur culpa deleniti molestiae fugit
-          libero quaerat? Consequatur commodi pariatur dolores aliquam, voluptas
-          ea officiis. Laboriosam adipisci eaque cupiditate.
+          {{ intro1 }}
         </div>
         <div class="w-full md:w-1/2 md:pl-15 text-muted">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-          pariatur assumenda odit doloribus ipsum ducimus laborum quibusdam
-          molestias labore, accusantium maxime repudiandae eveniet esse animi
-          similique numquam tenetur suscipit dolor.
+          {{ intro2 }}
         </div>
       </div>
       <div class="flex flex-col w-full md:flex-row">
@@ -132,26 +126,27 @@
           </div>
         </div>
         <div class="pt-16">
-          <div class="relative pt-5 pl-16 time-content">
+          <div v-for="exp in experience" :key="exp.title" class="relative pt-5 pl-16 time-content">
             <div>
               <h6
                 class="mb-1 text-base font-semibold text-white"
                 :class="{'text-dark': state.theme === 'light'}"
-              >WebDesigner – Mutation Media</h6>
+              >{{ exp.title }}</h6>
               <div class="mb-1 text-sm text-muted">
                 <span :class="{'text-dark': state.theme === 'light'}">
-                  <fa class="inline-block w-3 pb-1" icon="calendar"></fa> Feb 15th -
+                  <fa class="inline-block w-3 pb-1" icon="calendar"></fa> {{exp.startDate}} -
                   <span
+                    v-if="exp.current"
                     class="px-2 py-1 shadow-app-inner rounded-2"
                     :class="{'shadow-app-light-inner': state.theme === 'light'}"
                   >Current</span>
+                  <span v-else>
+                    {{exp.endDate}}
+                  </span>
                 </span>
               </div>
-              <p class="text-muted">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
-                ut sint voluptas rem? At nisi obcaecati placeat adipisci nobis?
-                Quidem rerum molestias ipsam vero fugiat sint porro in maxime
-                id?
+              <p v-for="(summary, i) in exp.summary" :key="i" class="text-muted">
+                {{summary}}
               </p>
             </div>
           </div>
@@ -253,10 +248,77 @@ export default {
         value: 'live:nwokikesamson'
       },
     ];
+    const intro1 = 'Detail-oriented and highly competent software developer with 5+ years’ experience developing a wide range of web and mobile tools. Possess in-depth understanding of the software ';
+    const intro2 = 'development lifecycle and very capable at all aspects of development from project planning to requirements gathering through to writing and testing code, documentation and support.';
+    const experience = [
+      {
+        title: 'Software Engineer(contract) - SBSC: Remote',
+        startDate: 'OCT 2020',
+        endDate: 'OCT 2020',
+        current: true,
+        summary: [
+          'Built UI components in React.js and other frontend technologies to digitize the process of global trade.',
+          'Improved output on project by increasing project completion rate by over 20% in one month, meeting deadlines and improving client satisfaction.'
+        ]
+      },
+      {
+        title: 'Web Developer - Softescu: Remote',
+        startDate: 'MAR 2020',
+        endDate: 'OCT 2020',
+        current: false,
+        summary: [
+          'Collaborated with various teams in designing, building and maintenance of client applications for different web/mobile solutions with a satisfaction rate of over 80%.',
+          'Extensively used Angular, Ionic and Scss to build client and company applications, in the process I was opportune to maintain and improve legacy Angular projects like “Openstory”',
+          'Built responsive and reusable Angular components to create an abstraction over Drupal CSM platform.'
+        ]
+      },
+      {
+        title: 'Javascript Engineer - Bloverse: Remote',
+        startDate: 'NOV 2018',
+        endDate: 'NOV 2019',
+        current: false,
+        summary: [
+          'Worked extensively on adding new features and improving the performance and accessibility of Bloverse as a platform.',
+          'Mentored and supported less experienced developers in the team helping them level up their technical skills which in turn improved the overall output of the team by over 40%.',
+        ]
+      },
+      {
+        title: 'Software Developer  - Rytegate Technologies',
+        startDate: 'FEB 2018',
+        endDate: 'NOV 2018',
+        current: false,
+        summary: [
+          `Led a team of three software developers to build Opsmanager version 2.0, maintain the company's in-house legacy code written in PHP and other SaaS.`,
+          'Reduced the cost of operation by 30% by automating processes and completely stopped financial leakages by building systems that track invoices, compare price changes and automatically send reports to the head of departments.',
+          'Awarded best staff in two FY quarters in a row.'
+        ]
+      },
+      {
+        title: 'Software Developer  - Prunedge Technologies',
+        startDate: 'FEB 2017',
+        endDate: 'JAN 2018',
+        current: false,
+        summary: [
+          `Built and maintained software applications for clients, interfacing and communicating with clients and colleagues with a client satisfaction rate of over 90%.`,
+          'Actively organised and participated in a training workshop with over 200+ participants that generated 70% of the company’s revenue for that month',
+          'Delivered a project that made the company $30,000.'
+        ]
+      },
+      {
+        title: 'Co-founder / Software Developer  - LodgeFriend',
+        startDate: 'SEP 2015',
+        endDate: 'DEC 2018',
+        current: false,
+        summary: [
+          `Built and maintained the UI and Database of an easy to use accommodation solution targeted at helping individuals easily find rental properties that fit their budget.`,
+          'Interfaced with clients in response to any difficulty faced using Lodgefriend and resolved them quickly. Serviced over 5000 clients within Nigeria.',
+        ]
+      },
+    ]
 
     const lastPersonalInfo =  computed(() =>  personalInfo.length - 1)
     const lastcontactInfo =  computed(() =>  contactInfo.length - 1)
-    return { state, personalInfo, contactInfo, lastPersonalInfo, lastcontactInfo }
+    return { state, personalInfo, contactInfo, lastPersonalInfo, lastcontactInfo, intro1, intro2, experience }
   }
 };
 </script>
