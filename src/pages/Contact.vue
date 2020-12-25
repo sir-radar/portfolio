403071a0b102986aebca7ea4a5abdc61d0940313
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-12-25 11:09:29 +0100

    update contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="about">
     <section id="header">
-      <h3 class="uppercase text-white text-base font-bold mb-2">
+      <h3
+        class="mb-2 text-base font-bold text-white uppercase"
+        :class="{'text-dark': state.theme === 'light'}"
+      >
         Map location
       </h3>
       <BigDivider class="mb-8" />
-      <div class="w-full py-4 relative">
-        <div class="shadow-app-inner p-2 h-388 rounded-1">
-          <div class="shadow-app w-full p-1 h-full rounded-1 relative">
+      <div class="relative w-full py-4">
+        <div
+          class="p-2 shadow-app-inner h-388 rounded-1"
+          :class="{'shadow-app-light-inner': state.theme === 'light'}"
+        >
+          <div
+            class="relative w-full h-full p-1 shadow-app rounded-1"
+            :class="{'shadow-app-light': state.theme === 'light'}"
+          >
             <iframe
               class="rounded-1"
               width="100%"
@@ -23,38 +32,70 @@
       </div>
     </section>
     <section class="info">
-      <h3 class="uppercase text-white text-base font-bold mb-2">
+      <h3
+        class="mb-2 text-base font-bold text-white uppercase"
+        :class="{'text-dark': state.theme === 'light'}"
+      >
         Contact info
       </h3>
       <BigDivider class="mb-8" />
     </section>
     <section class="send_email">
-      <h3 class="uppercase text-white text-base font-bold mb-2">
+      <h3
+        class="mb-2 text-base font-bold text-white uppercase"
+        :class="{'text-dark': state.theme === 'light'}"
+      >
         SEND ME AN EMAIL
       </h3>
       <BigDivider class="mb-8" />
       <form @submit.prevent="">
-        <div class="w-full flex mb-3">
+        <div class="flex w-full mb-3">
           <div class="w-1/2 pr-15">
-            <div class="w-full shadow-app rounded-2">
-              <input type="text" class="w-full form-control hover:shadow-app-inner focus:shadow-app-inner" placeholder="Name">
+            <div class="w-full shadow-app rounded-2" :class="{'shadow-app-light': state.theme === 'light'}">
+              <input
+                type="text"
+                class="w-full form-control hover:shadow-app-inner focus:shadow-app-inner"
+                :class="{'hover:shadow-app-light-inner focus:shadow-app-light-inner': state.theme === 'light'}"
+                placeholder="Name"
+              />
             </div>
           </div>
           <div class="w-1/2 pl-15">
-          <div class="w-full shadow-app rounded-2">
-            <input type="email" class="w-full form-control hover:shadow-app-inner focus:shadow-app-inner" placeholder="Email">
+          <div
+            class="w-full shadow-app rounded-2"
+            :class="{'shadow-app-light': state.theme === 'light'}"
+          >
+            <input
+              type="email"
+              class="w-full form-control hover:shadow-app-inner focus:shadow-app-inner"
+              :class="{'hover:shadow-app-light-inner focus:shadow-app-light-inner': state.theme === 'light'}"
+              placeholder="Email"
+            >
           </div>
           </div>
         </div>
-        <div class="w-full flex mb-3">
+        <div class="flex w-full mb-3">
           <div class="w-full">
-            <div class="w-full shadow-app rounded-2">
-              <textarea class="w-full form-control hover:shadow-app-inner focus:shadow-app-inner resize-n" placeholder="Message" rows="6" spellcheck="false">
+            <div
+              class="w-full shadow-app rounded-2"
+              :class="{'shadow-app-light': state.theme === 'light'}"
+            >
+              <textarea
+                class="w-full form-control hover:shadow-app-inner focus:shadow-app-inner resize-n"
+                :class="{'hover:shadow-app-light-inner focus:shadow-app-light-inner': state.theme === 'light'}"
+                placeholder="Message"
+                rows="6"
+                spellcheck="false">
               </textarea>
             </div>
           </div>
         </div>
-        <button class="shadow-app hover:shadow-app-inner rounded-2 py-2 px-6 font-medium text-submit outline-zero" type="submit">Send message</button>
+        <button
+          class="px-6 py-2 font-medium shadow-app hover:shadow-app-inner rounded-2 text-submit outline-zero"
+          :class="{'shadow-app-light hover:shadow-app-light-inner': state.theme === 'light'}"
+          style="color: var(--priColor)"
+          type="submit"
+        >Send message</button>
       </form>
     </section>
     <div class="flex flex-col items-center justify-center">
@@ -63,6 +104,7 @@
   </div>
 </template>
 <script>
+import {useState} from '../store';
 import BigDivider from "@/components/BigDivider";
 import BottomLinks from '@/components/BottomLinks';
 export default {
@@ -71,6 +113,11 @@ export default {
     BigDivider,
     BottomLinks
   },
+  setup(){
+    const { state } = useState();
+
+    return {state}
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -81,9 +128,9 @@ export default {
   font-size: 0.875rem;
   line-height: 1.7;
   color: #9da1a5;
-  background-color: #2c2d30;
+  background-color: var(--bgColor);
   background-clip: padding-box;
-  border: 1px solid #2c2d30;
+  border: 1px solid var(--bgColor);
   border-radius: 2rem;
   outline: 0;
   outline-color: initial;
