adbc53ba9807aa9f13e446a6348102f2b4d8b0ff
Samson Nwokike <samson.ikechukwu@softescu.com>
2021-01-02 01:29:32 +0100

    test print

--- src/components/BottomLinks.vue
+++ src/components/BottomLinks.vue
@@ -2,7 +2,7 @@
   <Divider class="mb-12 w-60" />
   <div class="flex flex-col justify-between w-full md:flex-row text-muted">
     <div class="px-4 mb-3 md:mb-0">
-      <a class="flex items-center p-0 mr-2 cursor-pointer">
+      <a href="/files/Samson_Nwokike_Resume.pdf" download class="flex items-center p-0 mr-2 cursor-pointer">
         <div class="mr-4 shadow-app rounded-2" :class="{'shadow-app-light': state.theme === 'light'}">
           <div
             class="flex items-center justify-center w-12 h-12 inner rounded-50 hover:shadow-app-inner"
@@ -17,7 +17,7 @@
         <div class="text-sm font-semibold uppercase">Download my cv</div>
       </a>
     </div>
-    <div class="px-4">
+    <div @click="print" class="px-4">
       <a class="flex items-center p-0 mr-2 cursor-pointer">
         <div class="mr-4 shadow-app rounded-2" :class="{'shadow-app-light': state.theme === 'light'}">
           <div
@@ -38,6 +38,7 @@
 
 <script>
 import { useState } from '../store';
+import printJS from 'print-js'
 import Divider from '@/components/Divider';
 import Download from '@/assets/images/svg/download.svg';
 import Print from '@/assets/images/svg/print.svg';
@@ -50,7 +51,12 @@ export default {
   },
   setup(){
     const { state } = useState();
-    return { state }
+
+    function print(){
+      printJS('/files/Samson_Nwokike_Resume.pdf')
+    }
+
+    return { state, print }
   }
 };
 </script>
