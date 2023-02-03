cc9a5166bf5d12b1b5f82268cbc6d97210aabf97
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-03 04:31:17 +0100

    move icons to constants folder

---
+++ src/constants/icons.js
@@ -0,0 +1,22 @@
+export const iconList = [
+  {
+    name: 'facebook-f',
+    class: 'w-2',
+    url: 'https://web.facebook.com/nwokike.samson.9',
+  },
+  {
+    name: 'github',
+    class: 'w-3',
+    url: 'https://github.com/sir-radar',
+  },
+  {
+    name: 'twitter',
+    class: 'w-3',
+    url: 'https://twitter.com/sir_radar',
+  },
+  {
+    name: 'linkedin',
+    class: 'w-3',
+    url: 'https://www.linkedin.com/in/sir-radar',
+  },
+];
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,18 @@
   <div class="flex flex-col items-center justify-center text-white home">
     <h1
       class="mb-1 font-semibold text-center text-title mt-21"
-      :class="{'text-dark': state.theme === 'light'}"
-      >
+      :class="{ 'text-dark': state.theme === 'light' }"
+    >
       Samson Nwokike
     </h1>
     <div class="relative mb-6 text-sm md:text-xl text-muted">
       <h1 class="h-5">
-        <a href="" id="typewrite" data-period="3000" data-type='[ "Hi, there ✌.", "Samson here!", "I am a Frontend developer", "with Fullstack development experience.", "Humble, Curious, Empathetic and Productive", "A Chelsea Fan." ]'>
+        <a
+          href=""
+          id="typewrite"
+          data-period="3000"
+          data-type='[ "Hi, there ✌.", "Samson here!", "I am a Frontend developer", "with Fullstack development experience.", "Humble, Curious, Empathetic and Productive", "A Chelsea Fan." ]'
+        >
           <span class="uppercase wrap"></span>
         </a>
       </h1>
@@ -18,12 +23,14 @@
         v-for="icon in iconList"
         :key="icon.name"
         class="mr-2 cursor-pointer shadow-app rounded-2"
-        :class="{'hover:shadow-app-light shadow-app-light': state.theme === 'light'}"
-        >
+        :class="{
+          'hover:shadow-app-light shadow-app-light': state.theme === 'light',
+        }"
+      >
         <a
           ref="icon"
           class="flex items-center justify-center w-10 h-10 p-0 hover:shadow-app-inner rounded-2"
-          :class="{'hover:shadow-app-light-inner': state.theme === 'light'}"
+          :class="{ 'hover:shadow-app-light-inner': state.theme === 'light' }"
           target="_blank"
           :href="icon.url"
         >
@@ -38,9 +45,10 @@
 </template>
 
 <script>
-import {onMounted, reactive} from 'vue';
+import { onMounted, reactive } from 'vue';
 import { useState } from '../store';
 import BottomLinks from '@/components/BottomLinks';
+import { iconList } from '../constants/icons';
 export default {
   name: 'Home',
   components: {
@@ -54,33 +62,18 @@ export default {
       if (toRotate) {
         TxtType(elements, JSON.parse(toRotate), period);
       }
-    })
+    });
     const { state } = useState();
-    const app =  reactive({ toRotate: '', el: null, loopNum: 0, period: 0, txt:'', isDeleting: false });
-    const iconList = [
-      {
-        name: 'facebook-f',
-        class: 'w-2',
-        url: 'https://web.facebook.com/nwokike.samson.9'
-      },
-      {
-        name: 'github',
-        class: 'w-3',
-        url: 'https://github.com/sir-radar'
-      },
-      {
-        name: 'twitter',
-        class: 'w-3',
-        url: 'https://twitter.com/sir_radar'
-      },
-      {
-        name: 'linkedin',
-        class: 'w-3',
-        url: 'https://www.linkedin.com/in/sir-radar'
-      },
-    ]
+    const app = reactive({
+      toRotate: '',
+      el: null,
+      loopNum: 0,
+      period: 0,
+      txt: '',
+      isDeleting: false,
+    });
 
-    function TxtType (el, toRotate, period) {
+    function TxtType(el, toRotate, period) {
       app.toRotate = toRotate;
       app.el = el;
       app.loopNum = 0;
@@ -90,30 +83,31 @@ export default {
       app.isDeleting = false;
     }
 
-    function tick () {
+    function tick() {
       var i = app.loopNum % app.toRotate.length;
       var fullTxt = app.toRotate[i];
 
       if (app.isDeleting) {
-      app.txt = fullTxt.substring(0, app.txt.length - 1);
+        app.txt = fullTxt.substring(0, app.txt.length - 1);
       } else {
-      app.txt = fullTxt.substring(0, app.txt.length + 1);
+        app.txt = fullTxt.substring(0, app.txt.length + 1);
       }
 
-      app.el.innerHTML = '<span class="wrap">'+app.txt+'</span>';
-
+      app.el.innerHTML = '<span class="wrap">' + app.txt + '</span>';
 
       var delta = 200 - Math.random() * 100;
 
-      if (app.isDeleting) { delta /= 2; }
+      if (app.isDeleting) {
+        delta /= 2;
+      }
 
       if (!app.isDeleting && app.txt === fullTxt) {
-      delta = app.period;
-      app.isDeleting = true;
+        delta = app.period;
+        app.isDeleting = true;
       } else if (app.isDeleting && app.txt === '') {
-      app.isDeleting = false;
-      app.loopNum++;
-      delta = 500;
+        app.isDeleting = false;
+        app.loopNum++;
+        delta = 500;
       }
 
       setTimeout(function() {
@@ -121,8 +115,7 @@ export default {
       }, delta);
     }
 
-    return { state, iconList }
-
-  }
+    return { state, iconList };
+  },
 };
 </script>
